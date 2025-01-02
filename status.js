2ce24569870e0f88a9e62d9a838924bf270fa80b
sophie <onmyterms64@gmail.com>
2025-01-02 03:57:51 -0500

    Add links to 88x31s

---
+++ status.js
@@ -0,0 +1,23 @@
+setInterval(async () => {
+    const res = await fetch(
+        'https://api.lanyard.rest/v1/users/1054919561834672160'
+    );
+    const req = await res.json();
+    let dstat = document.getElementById('dc_stat');
+    // noinspection JSUnresolvedReference
+    dstat.innerHTML = `[${req.data.discord_status}]`;
+
+    function getPropertyValue(property) {
+        return getComputedStyle(document.documentElement).getPropertyValue(`--${property}`);
+    }
+
+    const statusColors = {
+        online: getPropertyValue('green'),
+        idle: getPropertyValue('yellow'),
+        dnd: getPropertyValue('red'),
+        offline: getPropertyValue('grey')
+    };
+
+    // noinspection JSUnresolvedReference
+    dstat.style.color = statusColors[req.data.discord_status];
+}, 1000);
